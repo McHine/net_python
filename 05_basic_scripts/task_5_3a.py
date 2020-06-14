21c8670922cea90b51daddc09bd664bb08a0c10f
Nikola Koval <nikko@vivaldi.net>
2020-06-14 23:00:00 +0300

    5.3a done

---
+++ 05_basic_scripts/task_5_3a.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Задание 5.3a
+
+Дополнить скрипт из задания 5.3 таким образом, чтобы, в зависимости от выбранного режима,
+задавались разные вопросы в запросе о номере VLANа или списка VLANов:
+* для access: 'Введите номер VLAN:'
+* для trunk: 'Введите разрешенные VLANы:'
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+То есть эту задачу можно решить без использования условия if и циклов for/while.
+"""
+
+access_template = [
+    "switchport mode access",
+    "switchport access vlan {}",
+    "switchport nonegotiate",
+    "spanning-tree portfast",
+    "spanning-tree bpduguard enable",
+]
+
+trunk_template = [
+    "switchport trunk encapsulation dot1q",
+    "switchport mode trunk",
+    "switchport trunk allowed vlan {}",
+]
+
+general_template = {"access": access_template, "trunk": trunk_template}
+
+interface_mode = input("Введите режим работы интерфейса (access/trunk): ")
+
+interface_mode_switch = {'access': 'Введите номер VLAN: ', 'trunk':'Введите разрешенные VLANы: '}
+
+type_number_intetrface = input("Введите тип и номер интерфейса: ")
+vlan_number = input("{}".format(interface_mode_switch[interface_mode]))
+
+print("interface {}".format(type_number_intetrface))
+print('\n'.join(general_template[interface_mode]).format(vlan_number))
