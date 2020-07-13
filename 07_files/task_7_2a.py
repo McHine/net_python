ca3959008444776e3fdcd4084b28d1e874c7d07f
Nikola Koval <nikko@vivaldi.net>
2020-07-13 22:37:20 +0300

    7.2a is not so simple

--- 07_files/task_7_2a.py
+++ 07_files/task_7_2a.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Задание 7.2a
@@ -5,11 +6,23 @@
 Сделать копию скрипта задания 7.2.
 
 Дополнить скрипт:
-  Скрипт не должен выводить команды, в которых содержатся слова,
-  которые указаны в списке ignore.
+    Скрипт не должен выводить команды, в которых содержатся слова,
+    которые указаны в списке ignore.
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
 
 ignore = ["duplex", "alias", "Current configuration"]
+
+# from sys import argv
+# filename = argv[1]
+
+
+outf = open('07_files/config_sw1.txt', 'r')
+for line in outf:
+    if line.startswith("!"):
+        continue
+    elif 
+            
+                
