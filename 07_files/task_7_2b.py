b69de522653c271e9c7ce9980b4be7d0b7261f5c
Nikola Koval <nikko@vivaldi.net>
2020-07-14 22:38:17 +0300

    done with 7.2b

--- 07_files/task_7_2b.py
+++ 07_files/task_7_2b.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Задание 7.2b
@@ -14,3 +15,18 @@
 """
 
 ignore = ["duplex", "alias", "Current configuration"]
+
+from sys import argv
+filename = argv[1]
+file_for_write = open('config_sw1_cleared.txt', 'w')
+
+outf = open(filename, 'r')
+for line in outf:
+    ignore_string = False
+    for element in ignore:
+        if element in line:
+            ignore_string = True
+    if ignore_string == False:
+        file_for_write.write(line)
+        
+file_for_write.close()               
